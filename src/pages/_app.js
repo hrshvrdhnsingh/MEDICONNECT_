fab690a036c2982e125315458464667fe1f88cff
SoumojjalSen <soumojjalsen@gmail.com>
2025-02-02 19:22:56 +0530

    Pings DiseasePredictionApi

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,23 @@
-import "@/styles/globals.css";
-import { NextUIProvider } from "@nextui-org/react";
+import '@/styles/globals.css';
+import { NextUIProvider } from '@nextui-org/react';
+import { useEffect } from 'react';
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    fetch('https://diseasepredictionapi.onrender.com')
+      .then(() => console.log('ping successful'))
+      .catch(() => console.log('ping error'));
+  }, []);
+
   return (
     <NextUIProvider>
-      <div style={{ backgroundColor: "#182f5d", minHeight: "100vh", width: "100vw" }}>
+      <div
+        style={{
+          backgroundColor: '#182f5d',
+          minHeight: '100vh',
+          width: '100vw',
+        }}
+      >
         <Component {...pageProps} />
       </div>
     </NextUIProvider>
