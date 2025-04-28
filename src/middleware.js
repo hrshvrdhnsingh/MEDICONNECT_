1bd5ad8d749fd28c52d3583f6367dd36ee3351c2
SoumojjalSen <soumojjalsen@gmail.com>
2025-04-28 12:22:25 +0530

    Added middleware

---
+++ src/middleware.js
@@ -0,0 +1,26 @@
+import { NextResponse } from 'next/server';
+
+export function middleware(req) {
+  const allowedRoutes = [
+    '/',
+    '/diseasePrediction',
+    '/nearestPharmacies',
+    '/nearestHospitals',
+    '/medicineDetails',
+    '/nutritionChart',
+  ];
+
+  const { pathname } = req.nextUrl;
+
+  // Allow static files and API routes to bypass middleware
+  if (pathname.startsWith('/_next') || pathname.startsWith('/api') || pathname.startsWith('/static')) {
+    return NextResponse.next();
+  }
+
+  if (!allowedRoutes.includes(pathname)) {
+    // console.log('Redirecting to /'); // Log redirection
+    return NextResponse.redirect(new URL('/', req.url));
+  }
+
+  return NextResponse.next();
+}
